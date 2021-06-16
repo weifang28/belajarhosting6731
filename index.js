3f087fc5d5fbd951a3b0155fe5ea3dcdc3eb643f
weifang28 <60765003+weifang28@users.noreply.github.com>
2021-06-16 20:06:37 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+//token: 7afe1964d88c492499945c039c3f51d3
+var express = require("express");
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.set('view engine','ejs');
+
+app.listen(3000, function(){
+    console.log("3000 hehehe");
+});
+
+const users = require("./routes/users");
+app.use("/",users);
